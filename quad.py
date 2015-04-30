5e9406703b436a0dfc667a4beef132ab1c86e2cb
victorclemt <victor.clemt@gmail.com>
2015-04-30 12:26:41 -0500

    Cuadruplos
    Se anexa la primera versión beta de los cuadruplos ala cual se le harán
    pruebas.

--- quad.py
+++ quad.py
@@ -1,44 +1,45 @@
 
 cont=1
 contemp=0
-cuadruplos = [300][5]
+cuadruplos = [[0 for x in range(5)] for x in range(300)] 
 cuadro = 2
 i=0
 j=0
 
-
-
-Stack *PilaO;
-Stack *PSaltos;
-Stack *POper;
-def init(): 
 #Crear las pilas e inicializarlas
 #pila de operadores
-  PilaO=createStack(100); #pila de operandos
-  PSaltos=createStack(100); #pila de saltos
-  POper=createStack(100); #pila de operadores
-  cuadf = fopen("Maquina/cuadruplos.txt", "w+");
+PilaO = [] #pila de operandos
+PSaltos = [] #pila de saltos
+POper = [] #pila de operadores
+
+def init(): 
+
+  # PilaO=createStack(100); #pila de operandos
+  # PSaltos=createStack(100); #pila de saltos
+  # POper=createStack(100); #pila de operadores
+  cuadf = open(Maquina/cuadruplos.txt, 'w')
   if cuadf == NULL:
-    fprintf(stderr, "Archivo cuadruplos.txt no se ha podido abrir");
-    exit(1);
+    print(stderr, "Archivo cuadruplos.txt no se ha podido abrir");
+    sys.exit(1);
    
 def printcuadruplos():
     for i in range(300):
         for j in range(5):
           if cuadruplos[i][j]== 0:
-              sys.exit(1)
+              exit(1)
         print( repr(cuadf), repr(cuadruplos[i][j]))
     print(repr(cuadf)) ;
   
-#verificar dir como palabra resevada de python
-def escribe_ctei(dir, val):
-  print(repr(cuadf),"-1", repr(dir), repr(val))
 
-def escribe_ctef(dir, val):
-  print(repr(cuadf),"-1", repr(dir), repr(val))
+#impresion de de direccion en memoria y de valor asignado de la variable dependiendo de su tipo  
+def escribe_ctei(dire, val):
+  print(repr(cuadf),"-1", repr(dire), repr(val))
 
-def escribe_ctes(dir, val):
-  print(repr(cuadf),"-1", repr(dir), repr(val))
+def escribe_ctef(dire, val):
+  print(repr(cuadf),"-1", repr(dire), repr(val))
+
+def escribe_ctes(dire, val):
+  print(repr(cuadf),"-1", repr(dire), repr(val))
   
   
 def escribe_cuad(cuadro, ope, oper1, oper2, res):
@@ -53,31 +54,31 @@ def escribe_cuad(cuadro, ope, oper1, oper2, res):
 
 #1
 def pila_id(oper):
-  push(PilaO, oper)
-  print(stderr, "Ya meti oper \n", repr(oper))
+    PilaO.append(oper)
+    print(stderr, "Ya meti oper \n", repr(oper))
    
 #2
 def pila_op(op):
-    push(POper, op)
+    POper.append(op)
     print(stderr, "ya meti op \n", repr(op))
    
 #3
 def parentesisPush():
-    push(POper, 11) #genera pared falsa en pila
+    POper.append(11)  #genera pared falsa en pila
    
 def parentesisPop():
-    pop(POper) #saca de la pila  
+    POper.pop #saca de la pila  
       
 #5
 def termino(): 
   print(stderr, "Estoy en cuadruplo termino\n")
-  op = top(POper)
-  pop(POper)
+  op = POper[-1]
+  POper.pop
   if op == 3 or op == 4:      #operadores * o /
-    oper2 = top(PilaO) 
-    pop(PilaO)
-    oper1 = top(PilaO)
-    pop(PilaO)
+    oper2 = PilaO[-1]
+    PilaO.pop
+    oper1 = PilaO[-1]
+    PilaO.pop
     if (check(op, oper1, oper2)):   #checa si es valido
       escribe_cuad(cont, op, oper1, oper2, contemp+tipotemp())  #genera el cuadruplo
       pila_id(contemp+tipotemp())     #mete el resultado a la pila de operadores
@@ -85,7 +86,7 @@ def termino():
     
     else: #marca error si no es compatible
       print(stderr, "***ERROR DE TIPOS**** termino\n")
-      sys.exit(1)
+      exit(1)
   else:
       fprintf(stderr, "No es termino pasar al siguiente \n")
       pila_op(op)
@@ -93,12 +94,12 @@ def termino():
 #6
 def expresion():
   print(stderr, "Estoy en cuadruplo expresion\n")
-  op = top(POper);
-  pop(POper);
+  op = PilaO[-1]
+  POper.pop
   if op == 1 or op == 2:        #operadores + o -
-    oper2 = top(PilaO);
-    pop(PilaO);
-    oper1 = top(PilaO);
+    oper2 = PilaO[-1]
+    PilaO.pop
+    oper1 = PilaO[-1]
     pop(PilaO);
     if check(op, oper1, oper2):   #checa el tipo    
       escribe_cuad(cont, op, oper1, oper2,  (contemp+tipotemp())) #genera el cuadruplo
@@ -106,7 +107,7 @@ def expresion():
       contemp = contemp+1
     else:   #marca error si no es compatible
       print(stderr, "***ERROR DE TIPOS**** expresion\n")
-      sys.exit(1);
+      exit(1)
   else:
     print(stderr, "No es expresion pasar al siguiente \n")
     pila_op(op)
@@ -114,13 +115,13 @@ def expresion():
 #7
 def relacional():
     print(stderr, "Estoy en cuadruplo relacional\n");
-    op = top(POper)
-    pop(POper)
+    op = POper[-1]
+    POper.pop
     if op == 5 or op == 6 or op == 7 :  # op == < || op == > || op = ==
-        oper2 = top(PilaO) 
-        pop(PilaO)
-        oper1 = top(PilaO)
-        pop(PilaO)
+        oper2 = PilaO[-1] 
+        PilaO.pop
+        oper1 = PilaO[-1]
+        PilaO.pop
         if check(op, oper1, oper2):   #checa el tipo
           escribe_cuad(cont, op, oper1, oper2,  (contemp+tipotemp()))
           pila_id(contemp+tipotemp());      #mete el resultado a la pila de operadores
@@ -128,7 +129,7 @@ def relacional():
          
         else:   #marca error si no es compatible
           print(stderr, "***ERROR DE TIPOS**** relacional\n");
-          sys.exit(1);
+          exit(1);
 
     else:
         print(stderr, "No es relacional pasar al siguiente \n");
@@ -137,24 +138,24 @@ def relacional():
 #8
 def assign():
   print(stderr, "Estoy en quad assign\n")
-  op = top(POper)
-  pop(POper)
+  op = POper[-1] 
+  POper.pop
   if op == 10:  #si el operador es =
-    oper2 = top(PilaO) 
-    pop(PilaO); 
-    oper1 = top(PilaO)
-    pop(PilaO)
+    oper2 = PilaO[-1] 
+    PilaO.pop 
+    oper1 = PilaO[-1]
+    PilaO.pop
     if check(op, oper1, oper2): #checa el tipo
       escribe_cuad(cont, op,  oper2, -1, oper1)   #genera el cuadruplo
       contemp = contemp + 1 
     
     else:   #marca error si no es compatible
       print(stderr, "***ERROR DE TIPOS**** assign\n")
-      sys.exit(1);      
+      exit(1)
          
   else:
     print(stderr, "No es asignacion pasar al siguiente \n")
-    pila_op(op);
+    pila_op(op)
     
   #15
 def logico():
@@ -173,7 +174,7 @@ def logico():
     
       else:   #marca error si no es compatible
         print(stderr, "***ERROR DE TIPOS**** logico\n")
-        sys.exit(1)
+        exit(1)
          
     else:
       fprintf(stderr, "No es logico, pasar al siguiente \n")
@@ -185,49 +186,52 @@ def if1():
   #if
   #1.- genera gotoF y mete cont-1 a la pila de saltos
   op = 20
-  oper1 = top(PilaO)
+  oper1 = PilaO[-1]
   if per1 < 10000 or oper1>=11000:
     print (stderr, "***ERROR DE TIPOS**** IF\n")
   else:
-    pop(POper)
+    POper.pop
     escribe_cuad(cont, op, oper1, -1, -1)
-    push(PSaltos, cont-1)
+    PSaltos.append(cont-1)
     
 #11
 def else1():
 #2genera goto (else), y saca falso de la pila y rellena el primer salto
   op = 22 #goto
   escribe_cuad(cont, op, -1, -1, -1);
-  falso = top(PSaltos)
+  falso = PSaltos[-1]
   cuadruplos[falso-1][4]=cont
-  push(PSaltos, cont-1)
+  PSaltos.append(cont-1)
 #12
 def if2():
-  fin=top(PSaltos)
-  pop(PSaltos)
+  fin = PSaltos[-1]
+  PSaltos.pop
   cuadruplos[fin-1][4]=cont
   #13
 def do1():
   #do-while
   #1.- mete cont a pila de saltos
-  push(PSaltos, cont)
+  PSaltos.append(cont)
   #14
 def do2(): 
   #2.- genra gotoV e incrementa el cont
   op = 21 #gotov
-  oper1 = top(PilaO)
+  oper1 = Pila0[-1]
   oper2 = -1
   escribe_cuad(cont, op, oper1, -1, contemp)
   
-def print():
+def print1():
   op=33
-  push(POper,op)
-  
+  POper.append(op)
+
 def print2():
   oper1=top(PilaO)
   escribe_cuad(cont,top(POper),oper1,-1,-1)
-  cuadproc()
+
+def cuadproc():
   return cont
+
+
 
 
   
